bb43318796d630d5ed4ad855f42eb35c41ccc3fa
Dae-une <ko301201@naver.com>
2022-11-25 18:14:22 +0900

    feat : Binary Tree

---
+++ Data_Structure/Trees/Binary.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (value < current.value) {
+        if (!current.legth) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        return this;
+      }
+    }
+  }
+
+  contains(value) {
+    if (!this.root) return false;
+    let current = this.root;
+
+    while (current && !found) {
+      if ((value = current.value)) return true;
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+}
