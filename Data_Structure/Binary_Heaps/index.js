e275d7c79814bc451fbf0acc522f476d2d5460d1
Dae-une <ko301201@naver.com>
2022-11-28 18:27:25 +0900

    feat : Heap

---
+++ Data_Structure/Binary_Heaps/index.js
@@ -0,0 +1,61 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.value = [];
+  }
+
+  insert(elem) {
+    this.value.push(elem);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.value.length - 1;
+    const element = this.value[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.value[parentIdx];
+
+      if (element <= parent) break;
+
+      this.value[parentIdx] = element;
+      this.value[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMax() {
+    const max = this.value[0];
+    const end = this.value.pop();
+    if (this.value.length > 0) {
+      this.value[0] = end;
+      this.bubbleDown();
+    }
+    return max;
+  }
+  bubbleDown() {
+    let idx = 0;
+    const length = this.value.length;
+    const element = this.value[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.value[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.value[rightChildIdx];
+        if ((!swap && rightChild > element) || (swap && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.value[idx] = this.value[swap];
+      this.value[swap] = element;
+      idx = swap;
+    }
+  }
+}
